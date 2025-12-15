68fd9dc3cdf20a5572026f305cc30a7f9d9c0665
Hunter White <hpw6813@gmail.com>
2025-12-15 15:03:07 -0500

    set control button back to component

--- quilt-design/src/components/ControlButton.jsx
+++ quilt-design/src/components/ControlButton.jsx
@@ -1,10 +1,20 @@
 //These are the reusable buttons that will update grid size
-function ControlButton({ label, onClick }) {
-  return (
-    <button className="control-button" onClick={onClick}>
-      {label}
-    </button>
-  );
+import React, { Component } from "react";
+import './ControlButton.css';
+
+class ControlButton extends Component {
+  render() {
+    const { label, onClick, isPrimary, disabled } = this.props;
+    return (
+      <button 
+        className={`control-button ${isPrimary ? 'primary' : ''}`}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {label}
+      </button>
+    );
+  }
 }
 
 export default ControlButton;
